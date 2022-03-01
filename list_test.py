c3a8181341ef2d29dcff18fdd5942c611de8eed9
JanosGemes <gemes.abbc@gmail.com>
2022-03-01 16:50:48 +0100

    vége

--- list_test.py
+++ list_test.py
@@ -1,8 +1,11 @@
+from msilib.schema import CheckBox
+from tabnanny import check
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.select import select
 
 @pytest.fixture
 def driver():
@@ -26,3 +29,14 @@ def test_table_cells(driver: webdriver.Chrome):
     for element in elements:
         list_items.append(int(element.text)) 
     assert list_items == [100, 400, 150]
+
+def test_checkbox(driver: webdriver.Chrome):
+    CheckBox = driver.find_element_by_name("accept-licence")
+    CheckBox.click()
+    print("End")
+
+def test_select(driver: webdriver.Chrome):
+    select = Select(driver.find_element_by_name("car-type"))
+    select.select_by_visible_text("opel")
+    CheckBox.click()
+    print("End")
